1acd84b40a0202a19437c0f645eda2329e35a1fb
jellyfish <rvupmo33@gmail.com>
2024-06-11 15:22:05 -0400

    projects 1 and 2

---
+++ counter/counter.js
@@ -0,0 +1,17 @@
+let currentVal = document.getElementById("counter");
+let counter = 0;
+
+function increaseCounter(){
+  counter ++;
+  currentVal.textContent = counter;
+}
+
+function resetCounter(){
+  counter = 0;
+  currentVal.textContent = counter
+}
+
+function decreaseCounter(){
+  counter--;
+  currentVal.textContent = counter
+}
