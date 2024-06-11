95be07945269b72fa0f01e2c3a12819157b7a1f1
jellyfish <rvupmo33@gmail.com>
2024-06-11 15:39:53 -0400

    Day one completed

---
+++ 30DaysOfJS/DayOne/variable.js
@@ -0,0 +1,33 @@
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+const message = `Welcome!`;
+const isLoggedIn = true;
+let password;
+let emptyValue = null
+
+// Declare four variables without assigning values
+let var1;
+let var2;
+let var3;
+let var4;
+
+// Declare four variables with assigned values
+let var5 = 1;
+let var6 = 2;
+let var7 = 3;
+let var8 = 4;
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let fName = "Fname";
+let lName = "Lname";
+let maritalStatus = 'Mstatus';
+let country = 'Country';
+let age = 'Age';
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let fName2 = 'Fname2', lName2 = 'Lname2', maritalStatus2 = 'Mstatus2', country2 = 'Country2', age2 = 'Age2';
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 18;
+let yourAge = 19;
+
+console.log(`I am ${myAge} years old.\nYou are ${yourAge} years old.`);
