64ebdc9a8b574b125d96d266e54bdf276ac3f038
jellyfish <rvupmo33@gmail.com>
2024-06-15 23:15:00 -0400

    added tutorial files and completed uptil 04

---
+++ tutorialCode/unlockThePowerOfTheDom/03_innerText,textContent,innerHTML/live coding/app.js
@@ -0,0 +1,13 @@
+// innerText
+// textContent
+// innerHTML
+
+const p = document.querySelector("p");
+// console.log(p.innerText);
+// console.log(p.textContent);
+// console.log(p.innerHTML);
+
+const h1 = document.querySelector("h1");
+// h1.innerText = "This text was changed!";
+// h1.innerHTML = "<em>styled text!</em>";
+// h1.textContent = "Welcome!";
---
+++ tutorialCode/unlockThePowerOfTheDom/04_ClassList/app.js
@@ -0,0 +1,14 @@
+// classlist
+// add()
+// remove()
+// toggle()
+
+let h1Styles = document.querySelector("h1");
+console.log(h1Styles);
+
+h1Styles.classList.add("styles");
+
+h1Styles.classList.remove("styles");
+
+h1Styles.classList.toggle("newName");
+// adds a class
