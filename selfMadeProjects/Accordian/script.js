6e4314ace07cae83ef09698003c2e4b5cc3aff75
jellyfish <rvupmo33@gmail.com>
2024-06-23 23:09:22 -0400

    started accordian project

---
+++ selfMadeProjects/Accordian/script.js
@@ -0,0 +1,7 @@
+let accordian = document.querySelectorAll(".accordian");
+let question = document.querySelector(".question")
+let answer = document.querySelector(".answer")
+
+question.addEventListener("click", function Show(e){
+  if(e.target.class)
+})
