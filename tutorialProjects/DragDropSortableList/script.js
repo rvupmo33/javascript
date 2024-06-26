bbd7d108a79f86d91b945bc820cb45d34864242b
jellyfish <rvupmo33@gmail.com>
2024-06-26 10:35:24 -0400

    finished sortable list project

---
+++ tutorialProjects/DragDropSortableList/script.js
@@ -0,0 +1,22 @@
+const items = document.querySelectorAll(".item")
+const sortableList = document.querySelector(".sortable-list")
+
+items.forEach(item => {
+  item.addEventListener("dragstart", (e) =>{
+    setTimeout(()=> item.classList.add("dragging"),0)
+  })
+  item.addEventListener("dragend", () => item.classList.remove("dragging"))
+})
+
+const initSortableList = (e) => {
+  e.preventDefault();
+  const draggingItem = sortableList.querySelector(".dragging")
+  const siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")]
+
+  let nextSibling = siblings.find(sibling => {
+    return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+  })
+  sortableList.insertBefore(draggingItem, nextSibling)
+}
+
+sortableList.addEventListener("dragover", initSortableList)
