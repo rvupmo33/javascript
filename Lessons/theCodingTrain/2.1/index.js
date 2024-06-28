573d223d95f5a5649d44e3b79b08a6227c551753
jellyfish <rvupmo33@gmail.com>
2024-06-28 19:22:16 -0400

    set up db

--- Lessons/theCodingTrain/2.1/index.js
+++ Lessons/theCodingTrain/2.1/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const Datastore = require("nedb");
+
 const app = express();
 app.listen(3000, () => console.log("Server is listening on port 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
+
+const database = new Datastore("database.db");
+database.loadDatabase();
+
 app.post("/api", (request, response) => {
-  console.log("Got a request!");
-  console.log(request.body);
+  console.log("I got a request!");
+  // console.log(request.body);
   const data = request.body;
+  const timeStamp = Date.now();
+  data.timeStamp = timeStamp;
+  database.insert(data);
   response.json({
     status: "success",
+    timestamp: timeStamp,
     latitude: data.lat,
     longitude: data.lon,
   });
